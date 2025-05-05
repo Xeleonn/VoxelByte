2573e0ff7872495177707b191f150137fd333ce0
Xeleonn <tylerjameseaker@gmail.com>
2025-05-05 01:27:12 -0700

    Removed defunct file

--- build_mingw.bat
+++ build_mingw.bat
@@ -2,7 +2,7 @@
 @set OUT_DIR=Output
 @set OUT_EXE=VoxelByte
 @set INCLUDES=-I include/glad/ -I include/GLFW/
-@set SOURCES=VoxelByte/main.cpp include/glad/glad.c
+@set SOURCES=src/main.cpp include/glad/glad.c
 @set LIBS=-lopengl32 -lgdi32 -lglu32 -ldwmapi -L binaries -lglfw3
 mkdir %OUT_DIR%
 g++ -DUNICODE %INCLUDES% %SOURCES% -g -o %OUT_DIR%/%OUT_EXE%.exe --static -mwindows %LIBS%
