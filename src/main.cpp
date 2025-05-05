0a2f475e9f9476b21965af068fd4db81d02b9070
Kyle Meyer <vegas.kylem@gmail.com>
2025-05-05 03:12:54 -0700

    Modified shader loading

--- src/main.cpp
+++ src/main.cpp
@@ -52,7 +52,25 @@ int main()
 
     // build and compile our shader program
     // ------------------------------------
-    Shader ourShader("src/shaders/shader.vs", "src/shaders/shader.fs"); // you can name your shader files however you like
+    const char *vertexShaderSource ="#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 ourColor;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "   ourColor = aColor;\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "in vec3 ourColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(ourColor, 1.0f);\n"
+    "}\n\0";
+
+    Shader ourShader(vertexShaderSource, fragmentShaderSource); // you can name your shader files however you like
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
@@ -112,7 +130,7 @@ int main()
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        ImGui::Begin("VoxelByte");
+        ImGui::Begin("VoxelByte", nullptr, ImGuiWindowFlags_NoSavedSettings);
         ImGui::Text("Voxel Byte window test");
         ImGui::End();
 
